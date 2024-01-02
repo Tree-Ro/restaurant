bfa1027e7bf5b3dd1035d1e3f57a24d1b11b5da5
David Steiner <david1.steiner@gmail.com>
2024-01-02 16:25:38 +0100

    Added further boilerplate and file structuring

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+//TAB SWITCHING LOGIC/RENDERING HERE
+class content {
+    //Initiates page on load
+    init() {
+        this.load(home());
+    }
+
+    //Removes all content on page
+    clear() {}
+
+    //Appends subpage content
+    load(tabName) {
+        this.clear();
+    }
+}
