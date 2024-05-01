054d8227f915d1c5df5cf4095448cdcf8e8a7b63
David Steiner <david1.steiner@gmail.com>
2024-05-01 12:02:25 +0200

    moved homePage module to it's own .js file

---
+++ src/homePage.js
@@ -0,0 +1,50 @@
+const homePage = {
+  createElements: function () {
+    this.subContainer = document.createElement('div');
+
+    this.titleMsg = document.createElement('h1');
+    this.titleMsg.textContent = "Fry's Fries!";
+
+    this.slogan1 = document.createElement('p');
+    this.slogan1.textContent =
+      "Food, in the end, in our own tradition, is something holy. It's not about nutrients and calories.";
+    this.slogan2 = document.createElement('p');
+    this.slogan2.textContent =
+      "It's about sharing. It's about honesty. It's about Identity";
+
+    this.viewMenuBtn = document.createElement('button');
+    this.viewMenuBtn.textContent = 'View Menu';
+  },
+
+  appendElements: function () {
+    this.subContainer.appendChild(this.titleMsg);
+
+    this.subContainer.appendChild(this.slogan1);
+    this.subContainer.appendChild(this.slogan2);
+    content.appendChild(this.subContainer);
+
+    content.appendChild(this.viewMenuBtn);
+    this.addBtnListener();
+  },
+
+  addBtnListener: function () {
+    this.viewMenuBtn.addEventListener('click', () => {
+      menuPage.render(); // Requires menuPage.js module
+    });
+  },
+
+  clearContent: function () {
+    for (let i = content.childNodes.length - 1; i >= 0; i--) {
+      const node = content.childNodes[i];
+      node.remove();
+    }
+  },
+
+  render: function () {
+    this.clearContent();
+    this.createElements();
+    this.appendElements();
+  },
+};
+
+export default homePage;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,9 @@
 import './style.css';
 import './img/doubledFries.png';
+import homePage from './homePage.js';
 
 document.body.style = 'backdrop-filter: blur(2px);';
 
 const content = document.querySelector('#content');
 
-let homePage = {
-  createElements: function () {
-    this.subContainer = document.createElement('div');
-
-    this.titleMsg = document.createElement('h1');
-    this.titleMsg.textContent = "Fry's Fries!";
-
-    this.slogan1 = document.createElement('p');
-    this.slogan1.textContent =
-      "Food, in the end, in our own tradition, is something holy. It's not about nutrients and calories.";
-    this.slogan2 = document.createElement('p');
-    this.slogan2.textContent =
-      "It's about sharing. It's about honesty. It's about Identity";
-
-    this.viewMenuBtn = document.createElement('button');
-    this.viewMenuBtn.textContent = 'View Menu';
-  },
-
-  appendElements: function () {
-    this.subContainer.appendChild(this.titleMsg);
-
-    this.subContainer.appendChild(this.slogan1);
-    this.subContainer.appendChild(this.slogan2);
-    content.appendChild(this.subContainer);
-
-    content.appendChild(this.viewMenuBtn);
-  },
-
-  render: function () {
-    clearContent();
-    this.createElements();
-    this.appendElements();
-  },
-};
-
 homePage.render();
-
-function clearContent() {
-  for (let i = content.childNodes.length - 1; i >= 0; i--) {
-    const node = content.childNodes[i];
-    console.log(node);
-    node.remove();
-  }
-}
